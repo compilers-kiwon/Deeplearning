474f5073d2f226f1f6837c485177c848b8e1c4ac
Kiwon Kwon <kkwon@uei.com>
2023-01-13 16:33:25 +0900

    Auto stop

--- wine.py
+++ wine.py
@@ -28,21 +28,9 @@ model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-MODEL_DIR = './model/'
-if not os.path.exists(MODEL_DIR) :
-    os.mkdir(MODEL_DIR)
+early_stopping_callback = EarlyStopping(monitor='val_loss',patience=100)
 
-modelpath = "./model/{epoch:02d}-{val_loss:.4f}.hdf5"
-checkpointer = ModelCheckpoint(filepath=modelpath,monitor='val_loss',
-                               verbose=1,save_best_only=True)
+model.fit(X,Y,validation_split=0.2,epochs=2000,
+          batch_size=500,callbacks=[early_stopping_callback])
 
-history = model.fit(X,Y,epochs=3500,batch_size=500,validation_split=0.33)
-
-y_vloss = history.history['val_loss']
-y_acc = history.history['accuracy']
-
-x_len = np.arange(len(y_acc))
-plt.plot(x_len,y_vloss,"o",c="red",markersize=3)
-plt.plot(x_len,y_acc,"o",c="blue",markersize=3)
-
-plt.show()
+print("\n Accuracy%.4f"%(model.evaluate(X,Y)[1]))
