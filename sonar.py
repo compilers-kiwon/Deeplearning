308d3d064d687a2b305e75023becaf0f95bf3813
Kiwon Kwon <kkwon@uei.com>
2023-01-13 14:59:47 +0900

    Split data into train and test sets

--- sonar.py
+++ sonar.py
@@ -1,12 +1,13 @@
 from    keras.models            import  Sequential
 from    keras.layers.core       import  Dense
 from    sklearn.preprocessing   import  LabelEncoder
+from    sklearn.model_selection import  train_test_split
 
 import  pandas      as  pd
 import  numpy       as  np
 import  tensorflow  as  tf
 
-np.random.seed(3)
+np.random.seed(0)
 tf.random.set_seed(3)
 
 df = pd.read_csv("dataset/sonar.csv",header=None)
@@ -19,6 +20,8 @@ e = LabelEncoder()
 e.fit(Y_obj)
 Y = e.transform(Y_obj)
 
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.3,random_state=0)
+
 model = Sequential()
 model.add(Dense(24,input_dim=60,activation='relu'))
 model.add(Dense(10,activation='relu'))
@@ -28,5 +31,5 @@ model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-model.fit(X,Y,epochs=200,batch_size=5)
-print("\n Accuracy=%.4f"%(model.evaluate(X,Y)[1]))
+model.fit(X_train,Y_train,epochs=200,batch_size=5)
+print("\n Accuracy=%.4f"%(model.evaluate(X_test,Y_test)[1]))
