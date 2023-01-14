434e2f0c6c6035ca6fb02e5352e23fc8572265a9
Kiwon Kwon <compilers@naver.com>
2023-01-14 22:22:44 +0900

    Apply convolution

--- number.py
+++ number.py
@@ -15,15 +15,40 @@ tf.random.set_seed(3)
 
 (X_train,Y_train),(X_test,Y_test) = mnist.load_data()
 
-X_train = X_train.reshape(X_train.shape[0],784).astype('float32')/255
-X_test = X_test.reshape(X_test.shape[0],784).astype('float32')/255
+X_train = X_train.reshape(X_train.shape[0],28,28,1).astype('float32')/255
+X_test = X_test.reshape(X_test.shape[0],28,28,1).astype('float32')/255
 
 Y_train = np_utils.to_categorical(Y_train,10)
 Y_test = np_utils.to_categorical(Y_test,10)
 
+# 28x28(=784) data
+# =>
+# Convolution Y1(32 filters,3x3) + relu
+# =>
+# Convolution Y2(64 filters,3x3) + relu
+# =>
+# MaxPooling Y3(pool size:2) + relu
+# =>
+# Dropout 25%
+# =>
+# Flatten
+# =>
+# Y4(128 nodes) + relu
+# =>
+# Dropout 50%
+# =>
+# Y_out(size:10) + softmax
+from keras.layers import Dropout,Flatten,Conv2D,MaxPooling2D
+
 model = Sequential()
-model.add(Dense(512,input_dim=784,activation='relu'))
+model.add(Conv2D(32,kernel_size=(3,3),
+          input_shape=(28,28,1),activation='relu'))
+model.add(Conv2D(64,(3,3),activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Dropout(0.25))
+model.add(Flatten())
 model.add(Dense(128,activation='relu'))
+model.add(Dropout(0.5))          
 model.add(Dense(10,activation='softmax'))
 
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
@@ -31,7 +56,7 @@ early_stopping_callback = EarlyStopping(monitor='val_loss',patience=10)
 
 history = model.fit(X_train,Y_train,
                     validation_data=(X_test,Y_test),
-                    epochs=100,batch_size=50,verbose=0,
+                    epochs=30,batch_size=200,verbose=1,
                     callbacks=[early_stopping_callback])
 
 print("\n Test Accuracy:%.4f"%(model.evaluate(X_test,Y_test)[1]))
